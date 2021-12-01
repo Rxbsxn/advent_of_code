6a03606796dbfc75afe076e4f29f37546b39e990
Robert Harężlak <harezlak.robert7@gmail.com>
2021-12-01 20:54:39 +0100

    feat: add second half of the quest

--- 1/main.rs
+++ 1/main.rs
@@ -2,21 +2,31 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
+    let mut calculated_window: [u16; 3] = [0; 3];
     let mut last_depth: Option<u16> = None;
     let mut increases_count: i32 = 0;
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
 
-    for line in reader.lines() {
+
+    for (idx, line) in reader.lines().enumerate() {
         let depth = line.unwrap().parse().unwrap();
+
+        calculated_window[idx % 3] = depth;
+
+        if idx < 2 {
+            continue;
+        }
+
+        let window_sum = calculated_window.iter().sum();
         if last_depth.is_none() {
             last_depth = Some(depth);
             continue;
         } else {
-            if depth > last_depth.unwrap() {
+            if window_sum > last_depth.unwrap() {
                 increases_count += 1;
             }
-            last_depth = Some(depth);
+            last_depth = Some(window_sum);
         }
     }
 
